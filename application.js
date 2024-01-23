b202a1a02ad4c16f3f5e66e808f0670afeca2f01
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-23 10:27:54 +0100

    Update application.js

--- application.js
+++ application.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form");  
+const form = document.querySelector("form");   
 const dayInput = document.getElementById("day");
 const monthInput = document.getElementById("month");
 const yearInput = document.getElementById("year");
